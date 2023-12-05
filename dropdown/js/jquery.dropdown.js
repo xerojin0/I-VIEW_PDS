ee84b2801d716fdc1fa9c77a63e271920ac86208
I&VIEW_Development <inviewcs@gmail.com>
2023-12-05 14:38:51 +0900

    cchee
    font modify

--- dropdown/js/jquery.dropdown.js
+++ dropdown/js/jquery.dropdown.js
@@ -121,10 +121,10 @@
 			if( !this.options.slidingIn ) {
 				this.opts
 					.eq( this.optsCount - 1 )
-					.css( { top : this.options.stack ? 9 : 0, left : this.options.stack ? 4 : 0, width : this.options.stack ? this.size.width - 8 : this.size.width, transform : 'none' } )
+					.css( { top : this.options.stack ? 9 : 0, width : this.options.stack ? this.size.width - 8 : this.size.width, transform : 'none' } )
 					.end()
 					.eq( this.optsCount - 2 )
-					.css( { top : this.options.stack ? 6 : 0, left : this.options.stack ? 2 : 0, width : this.options.stack ? this.size.width - 4 : this.size.width, transform : 'none' } )
+					.css( { top : this.options.stack ? 6 : 0, width : this.options.stack ? this.size.width - 4 : this.size.width, transform : 'none' } )
 					.end()
 					.eq( this.optsCount - 3 )
 					.css( { top : this.options.stack ? 3 : 0, left : 0, transform : 'none' } );
@@ -165,16 +165,8 @@
 				$( this ).css( {
 					opacity : 1,
 					top : self.options.rotated ? self.size.height + self.options.gutter : ( i + 1 ) * ( self.size.height + self.options.gutter ),
-					left : self.options.random ? Math.floor( Math.random() * 11 - 5 ) : 0,
 					width : self.size.width,
 					marginLeft : 0,
-					transform : self.options.random ?
-						'rotate(' + Math.floor( Math.random() * 11 - 5 ) + 'deg)' :
-						self.options.rotated ?
-							self.options.rotated === 'right' ?
-								'rotate(-' + ( i * 5 ) + 'deg)' :
-								'rotate(' + ( i * 5 ) + 'deg)'
-							: 'none',
 					transitionDelay : self.options.delay && Modernizr.csstransitions ? self.options.slidingIn ? ( i * self.options.delay ) + 'ms' : ( ( self.optsCount - 1 - i ) * self.options.delay ) + 'ms' : 0
 				} );
 
